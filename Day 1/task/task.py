2db57901cd9ef746c0bc4a512e86f9300e60348c
P-Flo-debug <paul.florio@bhcc.edu>
2025-07-20 18:29:02 -0400

    Day 01: Implement Band Name Generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("what's your pets name?\n")
+print("Your band name could be " + city +" " + pet)
